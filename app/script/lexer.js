461c2533602d6e7bcd8cb4b01703852ace50af40
Mailwork <144586372+MailWork256@users.noreply.github.com>
2024-05-19 15:27:41 +0100

    scripting the language!

---
+++ app/script/lexer.js
@@ -0,0 +1,20 @@
+/*
+let is going to be (letshit)
+const is going to be (setshit)
+equals is going to be (be)
+
+identifier too for variable
+
+we also need numbers and basic operators (+ - * /)
+
+and so much more!
+
+eg.
+letshit times be 5 * (5 - 2)
+screen.shit(times)
+
+_________________________
+
+We want to translate this into an array to be read by the parser
+[] 
+*/
