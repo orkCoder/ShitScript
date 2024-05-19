75d8d557b8cde4b907a184fa53ff1d006ed0680c
Mailwork <144586372+MailWork256@users.noreply.github.com>
2024-05-19 17:14:35 +0100

    new lexer implmentations for further improvements.

--- app/script/lexer.js
+++ app/script/lexer.js
@@ -30,6 +30,11 @@ const TokenType = {
   Const: "Const",
 };
 
+/** @type {Record<string, TokenType>} */
+const KEYWORDS = {
+    "let": TokenType.let
+}
+
 /**
  * @typedef {Object} Token
  * @property {string} value
@@ -56,17 +61,46 @@ function token(value, type) {
   };
 }
 
+function isalpha (src) {
+    return src.toUpperCase() != src.toLowerCase();
+}
+
+function isint (str) {
+    const c = str.charCodeAt(0);
+    const bounds = ['0'.charCodeAt(0), '9'.charCodeAt(0)]
+    return (c >= bounds && c <= bounds[1])
+}
+
 export function tokenize(sourceCode) {
   const tokens = [];
   const src = sourceCode.split("");
 
   while (src.length > 0) {
-    if (src[0] == '(') {
+    if (src[0] == "(") {
       tokens.push(token(src.shift(), TokenType.OpenParen));
-    } else if (src[0] == ')') {
+    } else if (src[0] == ")") {
       tokens.push(token(src.shift(), TokenType.CloseParen));
-    } else if (src[0] == '+' || src[0] == '-') {
-        tokens.push(token(src.shift(), TokenType.BinaryOperators))
+    } else if (src[0] == "+" || src[0] == "-" || src[0] == "*" || src[0] == "/") {
+      tokens.push(token(src.shift(), TokenType.BinaryOperators));
+    } else {
+        if (isint(src[0])) {
+            let num = ''
+            while (src.length > 0 && isint[0]) {
+                num += src.shift();
+            }
+
+            tokens.push(token(num, TokenType.Number))
+        } else if (isalpha(src[0])) {
+            let ident = ''
+            while (src.length > 0 && isalpha[0]) {
+                ident += src.shift();
+            }
+
+            const reserved = KEYWORDS[ident]
+            if (reserved == undefined) {
+                tokens.push(token(ident, TokenType.Identifier))
+            }
+        }
     }
   }
 
